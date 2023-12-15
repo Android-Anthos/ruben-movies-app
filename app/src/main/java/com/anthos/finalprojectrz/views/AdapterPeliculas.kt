f5386fc90bf3932949758b75a350847423462eb6
Ruben Z <ruben.zevallos.saavedra@gmail.com>
2023-12-15 13:37:37 -0300

    library add 3 changes new

--- app/src/main/java/com/anthos/finalprojectrz/views/AdapterPeliculas.kt
+++ app/src/main/java/com/anthos/finalprojectrz/views/AdapterPeliculas.kt
@@ -1,21 +1,20 @@
 package com.anthos.finalprojectrz.views
 
-import android.app.DownloadManager.Request
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.appcompat.view.menu.MenuView.ItemView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import antonkozyriatskyi.circularprogressindicator.CircularProgressIndicator
 import com.anthos.finalprojectrz.R
 import com.anthos.finalprojectrz.core.Constantes
 import com.anthos.finalprojectrz.models.PeliculaModel
-import com.anthos.finalprojectrz.viewmodels.PeliculasViewModel
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
-import com.google.android.material.progressindicator.CircularProgressIndicator
+
+
 
 class AdapterPeliculas(
     val context: Context,
@@ -42,11 +41,10 @@ class AdapterPeliculas(
             .into(holder.ivPoster)
 
              holder.pcIndicator.maxProgress = Constantes.MAX_CALIFICACION
-        holder.run { pcIndicator.setCurrentProgress(pelicula.votoPromedio.toDouble()) }
+             holder.pcIndicator.setCurrentProgress(pelicula.votoPromedio.toDouble())
     }
 
     override fun getItemCount(): Int {
         return listaPeliculas.size
     }
-
 }
--- app/src/main/java/com/anthos/finalprojectrz/views/MainActivity.kt
+++ app/src/main/java/com/anthos/finalprojectrz/views/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
+import com.anthos.finalprojectrz.R
 import com.anthos.finalprojectrz.databinding.ActivityMainBinding
 import com.anthos.finalprojectrz.viewmodels.PeliculasViewModel
 
@@ -20,28 +21,49 @@ class MainActivity : AppCompatActivity() {
 
         viewModel = ViewModelProvider(this)[PeliculasViewModel::class.java]
 
-    setupRecyclerView()
+        setupRecyclerView()
 
-        viewModel.listaPeliculas.observe(this){
+        viewModel.listaPeliculas.observe(this) {
             adapterPeliculas.listaPeliculas = it
             adapterPeliculas.notifyDataSetChanged()
         }
 
-        binding.cvCartelera.setOnClickListener{
+        binding.cvCartelera.setOnClickListener {
             viewModel.obtenerCartelera()
             cambiarColorBoton("car")
         }
 
-        binding.cvPopulares.setOnClickListener{
+        binding.cvPopulares.setOnClickListener {
             viewModel.obtenerPopulares()
             cambiarColorBoton("pop")
         }
+
+        viewModel.obtenerCartelera()
+
     }
 
     private fun setupRecyclerView() {
-        val layoutManager = GridLayoutManager(this,3)
+        val layoutManager = GridLayoutManager(this, 3)
         binding.rvPeliculas.layoutManager = layoutManager
         adapterPeliculas = AdapterPeliculas(this, arrayListOf())
         binding.rvPeliculas.adapter = adapterPeliculas
+    }
+
+    private fun cambiarColorBoton(boton: String) {
+        when (boton) {
+            "car" -> {
+                binding.cvCartelera.setCardBackgroundColor(resources.getColor(R.color.green))
+                binding.cvPopulares.setCardBackgroundColor(resources.getColor(R.color.blue))
+            }
+
+            "pop" -> {
+                binding.cvCartelera.setCardBackgroundColor(resources.getColor(R.color.blue))
+                binding.cvPopulares.setCardBackgroundColor(resources.getColor(R.color.green))
+
+            }
+
+        }
+
+
     }
 }
