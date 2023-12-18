4b5c8448de7c8751161aa467c5da0a9e9a9aee97
Ruben Z <ruben.zevallos.saavedra@gmail.com>
2023-12-18 11:08:41 -0300

    library run 4, logo &color changes new

--- app/src/main/java/com/anthos/finalprojectrz/viewmodels/PeliculasViewModel.kt
+++ app/src/main/java/com/anthos/finalprojectrz/viewmodels/PeliculasViewModel.kt
@@ -1,5 +1,6 @@
 package com.anthos.finalprojectrz.viewmodels
 
+
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -11,9 +12,12 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class PeliculasViewModel : ViewModel() {
+class PeliculasViewModel: ViewModel() {
     private var _listaPeliculas = MutableLiveData<List<PeliculaModel>>()
     val listaPeliculas: LiveData<List<PeliculaModel>> = _listaPeliculas
+
+
+
     fun obtenerCartelera() {
         viewModelScope.launch(Dispatchers.IO) {
             val response = RetrofitClient.webService.obtenerCartelera(Constantes.API_KEY)
@@ -22,14 +26,14 @@ class PeliculasViewModel : ViewModel() {
             }
         }
     }
-    fun obtenerPopulares(){
-        viewModelScope.launch(Dispatchers.IO){
+
+    fun obtenerPopulares() {
+        viewModelScope.launch(Dispatchers.IO) {
             val response = RetrofitClient.webService.obtenerPopulares(Constantes.API_KEY)
-            withContext(Dispatchers.Main){
+            withContext(Dispatchers.Main) {
                 _listaPeliculas.value = response.body()!!.resultados.sortedByDescending { it.totalVotos }
             }
         }
     }
-
 
 }
