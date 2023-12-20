9426bb2f96e39f4ea0599a79b02bc4cc49f8e6a3
Ruben Z <ruben.zevallos.saavedra@gmail.com>
2023-12-20 12:34:51 -0300

    Action bar error fixed and the color and letter of the action bar changed

--- app/src/main/java/com/anthos/finalprojectrz/MainActivity.kt
+++ app/src/main/java/com/anthos/finalprojectrz/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.anthos.finalprojectrz
 
-import android.os.Bundle
+
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
         setupNavController()
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        val colorTextToolbar = ContextCompat.getColor(this, R.color.red)
+        toolbar.setTitleTextColor(colorTextToolbar)
     }
 
     private fun setupNavController() {
